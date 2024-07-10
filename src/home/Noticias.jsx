7daa9c5a72f68ce3ad0e1568c6c9b21c993c3514
dbarretol <57958754+dbarretol@users.noreply.github.com>
2024-07-09 23:48:51 -0500

    Zero base

---
+++ src/home/Noticias.jsx
@@ -0,0 +1,28 @@
+function Noticias() {
+    return (
+        <section className="padded">
+            <div className="container">
+                <h2>Noticias</h2>
+                <div className="row">
+                    <article className="col">
+                        <h3>Subió el dólar</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet sint ducimus nulla repellat deserunt ratione, beatae provident eum animi voluptatibus, quae, esse obcaecati quasi corporis nobis ipsam! Facere adipisci necessitatibus vitae. Sequi.</p>
+                        <p>Aperiam eos ratione, eveniet incidunt tempore quibusdam magnam? Debitis, harum! Minima quaerat, molestias blanditiis provident at possimus, assumenda numquam culpa a debitis porro molestiae facilis commodi autem quibusdam vel, sequi perspiciatis ducimus?</p>
+                    </article>
+                    <article className="col">
+                        <h3>Bajó el bitcoin</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat nisi eos est illum dolorem excepturi in reprehenderit ex impedit fuga! Odio laborum nihil ipsa, sint ab laudantium illo dignissimos vel adipisci dolor!</p>
+                        <p>Quae blanditiis eos labore, quidem odio, itaque aliquid esse necessitatibus eligendi assumenda similique? Id numquam cupiditate explicabo enim ea sint laudantium temporibus iste tempore eos dolorem veritatis, itaque quas, eveniet rerum deleniti.</p>
+                    </article>
+                    <article className="col">
+                        <h3>Nuevo yacimiento de litio</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit recusandae ducimus dolorum provident quis sed cumque magnam voluptatem fuga, nemo saepe incidunt! Doloremque quos a tenetur libero dolorum laudantium accusantium minus recusandae.</p>
+                        <p>Alias delectus atque recusandae dolores. Dolorum a rerum nulla in aperiam at iste praesentium saepe sit repellendus voluptatum dolor harum, amet corporis, non voluptatibus sequi officia ipsa quibusdam? Veritatis aperiam quo possimus?</p>
+                    </article>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Noticias
